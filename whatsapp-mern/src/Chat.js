db05c57a479a3ac1bdaddc0f278a8df78c0d4bff
Sinyor <abdurahmanov.togrul@gmail.com>
2021-04-22 01:59:45 +0300

    axios file create  and  message listed in chat component

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -2,23 +2,23 @@ import React from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from "@material-ui/core";
 import AttachFileIcon from '@material-ui/icons/AttachFile';
-import MoreVertIcon from '@material-ui/icons/MoreVert'; 
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 
-function Chat() {
- 
+function Chat({ messages }) {
+
     return (
         <div className="chat">
-               <div className="chat__header">
-                   <Avatar />
-                   <div className="chat__headerInfo">
-                       <h3>Room name</h3>
-                       <p>Last seen at...</p>
-                   </div>
-                   <div className="chat__headerRight">
-                   <IconButton>
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
                         <SearchOutlinedIcon />
                     </IconButton>
                     <IconButton>
@@ -27,54 +27,42 @@ function Chat() {
                     <IconButton>
                         <MoreVertIcon />
                     </IconButton>
-                   </div>
-               </div>
+                </div>
+            </div>
 
-                <div className="chat__body">
-                    <p className="chat__message">
-                        <span className="chat__name">
-                            Toghrul
+            <div className="chat__body">
+
+                {
+                    messages.map((message) => {
+                        
+                        <p className={`chat__message ${message.received && "chat__reciever"}`}>
+                            <span className="chat__name">
+                                {message.name}
                         </span>
-                        This is a message
+                        {message.message}
                         <span className="chat__timestamp">
-                            {new Date().toUTCString()}
-                        </span>
-                    </p>
-                    <p className="chat__message chat__reciever">
-                        <span className="chat__name">
-                            Toghrul
-                        </span>
-                        This is a message
-                        <span className="chat__timestamp">
-                            {new Date().toUTCString()}
-                        </span>
-                    </p>
-                    <p className="chat__message">
-                        <span className="chat__name">
-                            Toghrul
-                        </span>
-                        This is a message
-                        <span className="chat__timestamp">
-                            {new Date().toUTCString()}
-                        </span>
-                    </p>
-                    
-                </div>
-       
-                <div className="chat__footer">
-                    <InsertEmoticonIcon />
-                    <form >
-                        <input type="text"
+                                {message.timestamp}
+                            </span>
+                        </p>
+                    })
+                } 
+
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form >
+                    <input type="text"
                         placeholder="Type a message"
-                        />
-                        <button
-                        type="submit" 
-                        >
-                            Send a message
+                    />
+                    <button
+                        type="submit"
+                    >
+                        Send a message
                         </button>
-                    </form>
-                    <MicIcon />
-                </div>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }
