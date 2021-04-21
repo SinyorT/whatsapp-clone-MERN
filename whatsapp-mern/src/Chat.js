4edc1de0652c3ea2fc92f643647f080458efc868
Sinyor <abdurahmanov.togrul@gmail.com>
2021-04-21 23:35:53 +0300

    chat html css generate

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,9 +1,80 @@
 import React from 'react'
 import './Chat.css'
+import { Avatar, IconButton } from "@material-ui/core";
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert'; 
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+
 function Chat() {
+ 
     return (
         <div className="chat">
-                <h1>I am a Chat</h1>
+               <div className="chat__header">
+                   <Avatar />
+                   <div className="chat__headerInfo">
+                       <h3>Room name</h3>
+                       <p>Last seen at...</p>
+                   </div>
+                   <div className="chat__headerRight">
+                   <IconButton>
+                        <SearchOutlinedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                   </div>
+               </div>
+
+                <div className="chat__body">
+                    <p className="chat__message">
+                        <span className="chat__name">
+                            Toghrul
+                        </span>
+                        This is a message
+                        <span className="chat__timestamp">
+                            {new Date().toUTCString()}
+                        </span>
+                    </p>
+                    <p className="chat__message chat__reciever">
+                        <span className="chat__name">
+                            Toghrul
+                        </span>
+                        This is a message
+                        <span className="chat__timestamp">
+                            {new Date().toUTCString()}
+                        </span>
+                    </p>
+                    <p className="chat__message">
+                        <span className="chat__name">
+                            Toghrul
+                        </span>
+                        This is a message
+                        <span className="chat__timestamp">
+                            {new Date().toUTCString()}
+                        </span>
+                    </p>
+                    
+                </div>
+       
+                <div className="chat__footer">
+                    <InsertEmoticonIcon />
+                    <form >
+                        <input type="text"
+                        placeholder="Type a message"
+                        />
+                        <button
+                        type="submit" 
+                        >
+                            Send a message
+                        </button>
+                    </form>
+                    <MicIcon />
+                </div>
         </div>
     )
 }
