da4575ebcb21c1ae8eafeff7c28f491b4dd5ea97
Sinyor <abdurahmanov.togrul@gmail.com>
2021-04-21 21:21:45 +0300

    append firebase

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOjcPs8BoKUuVLxqe7DSuErRZtBiGzxts",
+    authDomain: "whatsapp-mern-7aedf.firebaseapp.com",
+    projectId: "whatsapp-mern-7aedf",
+    storageBucket: "whatsapp-mern-7aedf.appspot.com",
+    messagingSenderId: "633311487803",
+    appId: "1:633311487803:web:fd611e4b9c8bed0573b91e"
+  };
